e7ee1f16e140d24909c7d8c55246659d8d1603e2
Lucia <flyttotoro@gmail.com>
2023-05-22 00:38:30 +0100

    Add files via upload

---
+++ postTweet.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 19 22:12:38 2023
+
+@author: fengluyu
+"""
+
+import time
+import tweepy
+
+# Replace these values with your own keys
+CONSUMER_KEY = "qGroZlB63TQPMgBCE3kGoRvLj"
+CONSUMER_SECRET = "lr3OCcF1pbLpy9NPmzDbGk4I5Fd6coT8pRzEOXTateZ21f9Jq7"
+ACCESS_KEY = "1578459696851292160-DpLY7Kvx1AvUQhnZDEb0nR4VeJFckY"
+ACCESS_SECRET = "QrFXzOrZqrutnfXf2GS2EL0D858lztpDYABfrLAwy54KW"
+
+
+def tweet_line_by_line(file_name):
+    # Authenticate to Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+    # Create API object
+    api = tweepy.API(auth)
+
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()  # Remove newline characters from end
+            if len(line) <= 280:  # Twitter max character count is 280
+                try:
+                    api.update_status(line)
+                    print(f"Tweeted: {line}")
+                except tweepy.TweepError as e:
+                    print(e.reason)
+                time.sleep(60*60*3)  # Sleep for 3 hours
+            else:
+                print("Line is more than 280 characters, cannot be tweeted")
+
+if __name__ == "__main__":
+    tweet_line_by_line('trumpwords.txt')
